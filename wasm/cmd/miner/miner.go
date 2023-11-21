5035b46c4ee07b3c93c928dfc4c35ed1307e1fc7
onee-only <kimww0306@gmail.com>
2023-11-21 19:57:31 +0900

    expose 취소

--- wasm/cmd/miner/miner.go
+++ wasm/cmd/miner/miner.go
@@ -1,18 +1,7 @@
 package main
 
-import (
-	"miner/pkg/network"
-	"syscall/js"
-)
-
 func main() {
+	
 
-	exposeWasmApis()
-
-	// wait for nothing
-	<-make(chan struct{})
-}
-
-func exposeWasmApis() {
-	js.Global().Set("initRTCConnection", network.InitRTCConn())
+	select {}
 }
