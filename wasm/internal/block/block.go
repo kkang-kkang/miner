f0fce60244fcdd358d7db2b5b4f8b1b5994288d5
onee-only <kimww0306@gmail.com>
2023-11-23 18:26:26 +0900

    block 추가

---
+++ wasm/internal/block/block.go
@@ -0,0 +1,127 @@
+package block
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"time"
+
+	"github.com/cbergoon/merkletree"
+	"github.com/pkg/errors"
+
+	"miner/internal/tx"
+)
+
+const (
+	BlockMiningPrize = 10
+)
+
+// Header is header part of the block.
+type Header struct {
+	CurHash  []byte `json:"curHash"`
+	PrevHash []byte `json:"prevHash"`
+
+	DataHash   []byte `json:"dataHash"`
+	Difficulty uint8  `json:"difficulty"`
+	Nonce      uint64 `json:"nonce"`
+
+	Timestamp time.Time `json:"timestamp"`
+}
+
+// Body is body part of the block.
+type Body struct {
+	CoinbaseTxHash []byte            `json:"coinbaseTxHash"`
+	TxHashes       [][]byte          `json:"txHashes"`
+	CoinbaseTx     *tx.Transaction   `json:"coinbaseTx,omitempty"`
+	Txs            []*tx.Transaction `json:"txs,omitempty"`
+}
+
+type Block struct {
+	Header *Header `json:"header"`
+	Body   *Body   `json:"body"`
+}
+
+// New creates new block from given arguments.
+// You still have to configure [nonce, hash, difficulty].
+func New(minerAddr []byte, txs []*tx.Transaction, prevHash []byte) (*Block, error) {
+	coinBaseTx := &tx.Transaction{
+		Inputs: []*tx.TxInput{},
+		Outputs: []*tx.TxOutput{{
+			Addr:   minerAddr,
+			Amount: BlockMiningPrize * uint64(len(txs)), // TODO: change this to actual algorithm.
+		}},
+	}
+
+	if _, err := coinBaseTx.CalculateHash(); err != nil {
+		return nil, errors.Wrap(err, "failed to create hash of coinbaseTx")
+	}
+
+	block := &Block{
+		Header: &Header{PrevHash: prevHash},
+		Body: &Body{
+			CoinbaseTxHash: coinBaseTx.Hash,
+			CoinbaseTx:     coinBaseTx,
+			Txs:            txs,
+			TxHashes:       make([][]byte, 0, len(txs)),
+		},
+	}
+
+	tree, err := block.CreateMerkleTree()
+	if err != nil {
+		return nil, errors.Wrap(err, "merkle tree creation failed")
+	}
+
+	block.Header.DataHash = tree.MerkleRoot()
+
+	for _, tx := range block.Body.Txs {
+		block.Body.TxHashes = append(block.Body.TxHashes, tx.Hash)
+	}
+
+	block.Header.Timestamp = time.Now()
+
+	return block, nil
+}
+
+func (h *Header) MakeHash() ([]byte, error) {
+	hash := sha256.New()
+
+	if _, err := hash.Write(h.PrevHash); err != nil {
+		return nil, errors.Wrap(err, "failed to write hash")
+	}
+
+	if _, err := hash.Write(h.DataHash); err != nil {
+		return nil, errors.Wrap(err, "failed to write hash")
+	}
+
+	if err := binary.Write(hash, binary.LittleEndian, h.Timestamp.UnixNano()); err != nil {
+		return nil, errors.Wrap(err, "failed to write hash")
+	}
+
+	if err := binary.Write(hash, binary.LittleEndian, h.Difficulty); err != nil {
+		return nil, errors.Wrap(err, "failed to write hash")
+	}
+
+	if err := binary.Write(hash, binary.LittleEndian, h.Nonce); err != nil {
+		return nil, errors.Wrap(err, "failed to write hash")
+	}
+
+	return hash.Sum(nil), nil
+}
+
+// CreateMerkleTree creates merkle tree from block's transactions.
+func (b *Block) CreateMerkleTree() (*merkletree.MerkleTree, error) {
+	nodes := make([]merkletree.Content, 0, len(b.Body.Txs)+1)
+
+	nodes = append(nodes, b.Body.CoinbaseTx)
+	for _, tx := range b.Body.Txs {
+		nodes = append(nodes, tx)
+	}
+	return merkletree.NewTree(nodes)
+}
+
+// ValidateDataHash validates that the block's dataHash(merkletree's root hash)
+// equals to the actual hash of calculated merkle tree.
+func (b *Block) ValidateDataHash() bool {
+	tree, err := b.CreateMerkleTree()
+	return err == nil && bytes.Equal(b.Header.DataHash, tree.MerkleRoot())
+}
---
+++ wasm/internal/block/block_test.go
@@ -0,0 +1,98 @@
+package block_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"miner/internal/block"
+	"miner/internal/tx"
+)
+
+func TestVerifyDataHash(t *testing.T) {
+	block := block.Block{
+		Header: &block.Header{},
+		Body: &block.Body{
+			CoinbaseTx: &tx.Transaction{
+				Inputs: []*tx.TxInput{
+					{
+						TxHash:    nil,
+						OutIdx:    2,
+						Signature: []byte("aefaelfkahflakehfalkejfhaljk"),
+					},
+				},
+				Outputs: []*tx.TxOutput{
+					{
+						Addr:   []byte("aefefaefaa"),
+						Amount: 32,
+					},
+				},
+			},
+			Txs: []*tx.Transaction{
+				{
+					Inputs: []*tx.TxInput{
+						{
+							TxHash:    nil,
+							OutIdx:    2,
+							Signature: []byte("aefaelfkahflakehfalkejfhaljk"),
+						},
+					},
+					Outputs: []*tx.TxOutput{
+						{
+							Addr:   []byte("aefefaefaa"),
+							Amount: 32,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	tree, err := block.CreateMerkleTree()
+	assert.NoError(t, err)
+
+	block.Header.DataHash = tree.MerkleRoot()
+
+	valid := block.ValidateDataHash()
+	assert.True(t, valid)
+}
+
+func TestNew(t *testing.T) {
+	myAddr := []byte("aefafe")
+	prevHash := []byte("dfskfjdshf")
+
+	txs := []*tx.Transaction{
+		{
+			Hash: []byte("asdfs"),
+			Inputs: []*tx.TxInput{{
+				TxHash:    []byte("afef"),
+				OutIdx:    1,
+				Signature: []byte("aefeadfsdaf"),
+			}},
+			Outputs: []*tx.TxOutput{{
+				Addr:   []byte("adafdsaf"),
+				Amount: 20,
+			}},
+		},
+		{
+			Hash: []byte("aefaefaf"),
+			Inputs: []*tx.TxInput{{
+				TxHash:    []byte("afef"),
+				OutIdx:    1,
+				Signature: []byte("aefeadfsdaf"),
+			}},
+			Outputs: []*tx.TxOutput{{
+				Addr:   []byte("adafdsaf"),
+				Amount: 20,
+			}},
+		},
+	}
+
+	b, err := block.New(myAddr, txs, prevHash)
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	valid := b.ValidateDataHash()
+	assert.True(t, valid)
+}
