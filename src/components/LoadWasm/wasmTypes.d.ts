4debe4ae01a1ccf05bbb6e290f6ec47b9afc9424
onee-only <kimww0306@gmail.com>
2023-11-27 23:21:40 +0900

    global type 추가

--- src/components/LoadWasm/wasmTypes.d.ts
+++ src/components/LoadWasm/wasmTypes.d.ts
@@ -1,10 +1,38 @@
 declare global {
+  export type TxCandidate = {
+    readonly amount: number;
+    readonly srcAddress: string;
+    readonly dstAddress: string;
+    readonly privateKey: string;
+  };
+
+  export type BlockCandidate = {
+    readonly transactionHashes: string[];
+  };
+
+  export type TxIn = {
+    txHash: string;
+    outIdx: number;
+    sig: string;
+  };
+
+  export type TxOut = {
+    addr: string;
+    amount: number;
+  };
+
+  export type Transaction = {
+    hash: string;
+    inputs: TxIn[];
+    outputs: TxOut[];
+  };
+
   export interface Window {
     Go: any;
-    createNewTx: () => Promise<void>;
-    createBlock: () => Promise<void>;
-    insertBroadcastedTx: () => Promise<void>;
-    insertBroadcastedBlock: () => Promise<void>;
+    createNewTx: (input: TxCandidate) => Promise<Transaction>;
+    createBlock: (input: BlockCandidate) => Promise<void>;
+    insertBroadcastedTx: (candidate: string) => Promise<void>;
+    insertBroadcastedBlock: (candidate: string) => Promise<void>;
   }
 }
 
--- src/components/LoadWasm/wasmWorker.ts
+++ src/components/LoadWasm/wasmWorker.ts
@@ -13,8 +13,24 @@ async function initWasmWorker() {
   onmessage = async (event: MessageEvent<Message<unknown>>): Promise<void> => {
     switch (event.data.type) {
       case MessageTypes.CREATE_TX: {
-        await self.createNewTx();
-        postMessage(MessageTypes.TX_CREATED, {});
+        const val = await self.createNewTx(event.data.data as TxCandidate);
+        postMessage(new Message(MessageTypes.TX_CREATED, val));
+        break;
+      }
+      case MessageTypes.CREATE_BLOCK: {
+        await self.createBlock(event.data.data as BlockCandidate);
+        postMessage(new Message(MessageTypes.BLOCK_CREATED, {}));
+        break;
+      }
+      case MessageTypes.INSERT_TX: {
+        await self.insertBroadcastedTx(event.data.data as string);
+        postMessage(new Message(MessageTypes.TX_INSERTED, {}));
+        break;
+      }
+      case MessageTypes.INSERT_BLOCK: {
+        await self.insertBroadcastedBlock(event.data.data as string);
+        postMessage(new Message(MessageTypes.BLOCK_INSERTED, {}));
+        break;
       }
     }
   };
