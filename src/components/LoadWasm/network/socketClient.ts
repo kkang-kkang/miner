146507f6f20a78606fbb05585a8a41adef1f418f
onee-only <kimww0306@gmail.com>
2023-12-12 17:07:14 +0900

    socket client 추가

--- src/components/LoadWasm/network/socketClient.ts
+++ src/components/LoadWasm/network/socketClient.ts
@@ -1,10 +1,16 @@
 import { Socket, io } from "socket.io-client";
-import { EventType, PeerEvent } from "./event";
-
-interface Message<T> {
-  requestId: string;
-  data: T;
-}
+import { DBManager, ObjectStore } from "../misc";
+import {
+  AddrPayload,
+  BalancePayload,
+  BlockQuery,
+  BlockSummary,
+  EventType,
+  HashPayload,
+  IDEvent,
+  PeerEvent,
+} from "./event";
+import { NetworkListener } from "./networkListener";
 
 enum Request {
   NEW_TX = "new-tx",
@@ -14,15 +20,21 @@ enum Request {
   LIST_BLOCK = "list-block",
 }
 
-export class SocketClient extends EventTarget {
+export class SocketClient {
   private readonly socket: Socket;
 
-  constructor() {
-    super();
+  constructor(
+    private readonly networkListener: NetworkListener,
+    private readonly dbManager: DBManager,
+    nickname: string,
+  ) {
     this.socket = io({
       host: "",
       port: 1111,
       transports: ["websocket"],
+      auth: {
+        nickname,
+      },
     });
     this.registerListeners();
   }
@@ -47,35 +59,73 @@ export class SocketClient extends EventTarget {
     this.socket.on(Request.GET_BALANCE, this.handleGetBalance);
     this.socket.on(Request.LIST_BLOCK, this.handleListBlock);
     this.socket.on(Request.NEW_TX, this.handleNewTx);
+
+    this.networkListener.attachListener(EventType.TX_CREATED, this.handleTxCreated);
   }
 
-  private handleFindBlock(): void {}
-  private handleFindTx(): void {}
-  private handleGetBalance(): void {}
-  private handleListBlock(): void {}
+  private async handleFindBlock({ data: { hash }, id }: IDEvent<HashPayload>) {
+    const block = await this.findBlock(hash);
 
-  private handleNewTx({ data: candidate, requestId }: Message<TxCandidate>): Promise<void> {
-    return new Promise((resolve) => {
-      const handleTxCreated = (e: Event) => {
-        this.removeEventListener(EventType.TX_CREATED, handleTxCreated);
+    const msg: IDEvent<Block | null> = { id, data: block };
+    this.socket.emit(Request.FIND_BLOCK, msg);
+  }
 
-        const { detail: tx } = e as CustomEvent<Transaction | null>;
+  private async handleFindTx({ data: { hash }, id }: IDEvent<HashPayload>): Promise<void> {
+    const tx = await this.dbManager.get(ObjectStore.TRANSACTION, hash);
 
-        const msg: Message<string> = {
-          requestId,
-          data: tx?.hash ?? "",
-        };
+    const msg: IDEvent<Transaction | null> = { id, data: tx ? (tx as Transaction) : null };
+    this.socket.emit(Request.FIND_TX, msg);
+  }
 
-        this.socket.emit(Request.NEW_TX, msg);
-        resolve();
-      };
+  private async handleGetBalance({ data: { addr }, id }: IDEvent<AddrPayload>): Promise<void> {
+    const balance = await this.dbManager.getBalance(addr);
 
-      this.addEventListener(EventType.TX_CREATED, handleTxCreated);
-      this.dispatchEvent(
-        new CustomEvent<TxCandidate>(EventType.NEW_TX, {
-          detail: candidate,
-        }),
-      );
-    });
+    const msg: IDEvent<BalancePayload> = { id, data: { balance } };
+    this.socket.emit(Request.GET_BALANCE, msg);
+  }
+
+  private async handleListBlock({ data: { count, head }, id }: IDEvent<BlockQuery>): Promise<void> {
+    const blockSummaries: BlockSummary[] = [];
+
+    for (let i = 0; i < count; i++) {
+      const block = await this.findBlock(head);
+      if (block == null || block.header.curHash === "00") break;
+      blockSummaries.push({
+        curHash: block.header.curHash,
+        prevHash: block.header.prevHash,
+        timestamp: block.header.timestamp,
+        txCount: block.body.txHashes.length + 1,
+      });
+      head = block.header.prevHash;
+    }
+
+    const msg: IDEvent<BlockSummary[]> = { data: blockSummaries, id };
+    this.socket.emit(Request.LIST_BLOCK, msg);
+  }
+
+  private handleNewTx(msg: IDEvent<TxCandidate>) {
+    this.networkListener.dispatch(EventType.NEW_TX, msg);
+  }
+
+  private handleTxCreated({ data: tx, id }: IDEvent<Transaction | null>) {
+    const msg: IDEvent<string> = {
+      id,
+      data: tx?.hash ?? "",
+    };
+
+    this.socket.emit(Request.NEW_TX, msg);
+  }
+
+  private async findBlock(hash: string): Promise<Block | null> {
+    const body = await this.dbManager.get(ObjectStore.BLOCK_BODIES, hash);
+    const header = await this.dbManager.get(ObjectStore.BLOCK_HEADERS, hash);
+    if (header === undefined || body === undefined) {
+      return null;
+    }
+
+    return {
+      body: body as BlockBody,
+      header: header as BlockHeader,
+    };
   }
 }
