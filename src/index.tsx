5043dc9e06d230553142ec1bb59c1ef972bc1318
onee-only <kimww0306@gmail.com>
2023-12-24 17:12:47 +0900

    index

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,19 @@
+import { Box, ChakraProvider } from "@chakra-ui/react";
 import { createRoot } from "react-dom/client";
-import App from "./components/App/App";
 import { LoadWasm } from "./components/LoadWasm";
+import PreEnter from "./components/PreEnter/PreEnter";
 import "./index.css";
+
 const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <LoadWasm>
-    <App />
-  </LoadWasm>,
+  <ChakraProvider>
+    <Box
+      height={"100vh"}
+      bgColor={"#1F2428"} /* bgGradient={"linear(to-t, green.500, blue.700)" } */
+    >
+      <LoadWasm>
+        <PreEnter />
+      </LoadWasm>
+    </Box>
+  </ChakraProvider>,
 );
